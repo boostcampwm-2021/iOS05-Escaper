5d1d79be8d3b84fe1c60ae4459a47c5083b55ce3
wansook <wansook0316@gmail.com>
2021-11-02 17:49:17 +0900

    Feat: Genre 종류 축소

--- Escaper/Escaper/Domain/DataStruct/Genre.swift
+++ Escaper/Escaper/Domain/DataStruct/Genre.swift
@@ -14,7 +14,6 @@ enum Genre: String {
     case action = "액션"
     case mystery = "미스터리"
     case thriller = "스릴러"
-    case adult = "19금"
     case drama = "드라마"
     case crime = "범죄"
     case fantasy = "판타지"
@@ -22,9 +21,5 @@ enum Genre: String {
     case romance = "로맨스"
     case comedy = "코미디"
     case adventure = "모험"
-    case arcade = "아케이드"
     case outdoor = "야외"
-    case music = "음악"
-    case infiltration = "잠입"
-    case scienceFiction = "SF"
 }
