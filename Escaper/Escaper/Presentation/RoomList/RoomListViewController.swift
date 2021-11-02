8fe0c00d2212d3a1b08baf286b86b85670a2bdf9
wansook <wansook0316@gmail.com>
2021-11-02 15:24:20 +0900

    Feat: 모두보기 화면 Interface 정의

--- Escaper/Escaper/Presentation/RoomList/RoomListViewController.swift
+++ Escaper/Escaper/Presentation/RoomList/RoomListViewController.swift
@@ -8,6 +8,14 @@
 import UIKit
 
 class RoomListViewController: UIViewController {
+    
+    private var viewModel: RoomListViewModelInterface!
+    
+    func create() {
+//        let repository = DefaultNetworkService()
+//        let useCase = DefaultRoomListUseCase(repository: repository)
+//        let viewModel = DefaultRoomListViewModel(usecase: useCase)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
