5dec8789a16d10cb93d39cec038641c0c69014a7
jeff <xorgus1234@gmail.com>
2021-11-03 17:09:38 +0900

    Fix: 방 파라미터 수정

--- Escaper/Escaper/Domain/DataStruct/Room.swift
+++ Escaper/Escaper/Domain/DataStruct/Room.swift
@@ -15,8 +15,8 @@ struct Room: Codable {
     var telephone: String
     var genres: [Genre]
     var geoLocation: GeoLocation
-    var address = ["서울특별시", "강남구", "역삼동"]
-    var playedUsers: [UserRecord]
+    var address: [String]
+    var userRecords: [UserRecord]
 
     var toDictionary: [String: Any] {
         let dictionary: [String: Any] = [
@@ -28,7 +28,7 @@ struct Room: Codable {
             "genres": self.genres.map { $0.rawValue },
             "geoLocation": self.geoLocation.toDictionary,
             "address": self.address,
-            "playedUsers": self.playedUsers.map { $0.toDictionary }
+            "userRecords": self.userRecords.map { $0.toDictionary }
         ]
 
         return dictionary
