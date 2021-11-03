e645b9b4b5b0a0ca439ea8767da8d8f2796668de
sunflower <rebecca_0922@naver.com>
2021-11-03 22:11:19 +0900

    Feat: TagButton 구현

---
+++ Escaper/Escaper/Presentation/RoomList/Views/TagButton.swift
@@ -0,0 +1,44 @@
+//
+//  TagButton.swift
+//  Escaper
+//
+//  Created by shinheeRo on 2021/11/03.
+//
+
+import UIKit
+
+class TagButton: UIButton {
+
+    private(set) var element: Tagable?
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.configure()
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.configure()
+    }
+
+    convenience init(element: Tagable) {
+        self.init(frame: .zero)
+        self.element = element
+        self.setTitle(element.name, for: .normal)
+    }
+
+    private func configure() {
+        self.layer.cornerRadius = 5
+        self.titleLabel?.textColor = UIColor(named: ColorPalette.skullWhite.code)
+        self.untouched()
+    }
+
+    func touched() {
+        self.backgroundColor = UIColor(named: ColorPalette.pumpkin.code)
+    }
+
+    func untouched() {
+        self.backgroundColor = UIColor(named: ColorPalette.bloodyDarkBurgundy.code)
+    }
+
+}
