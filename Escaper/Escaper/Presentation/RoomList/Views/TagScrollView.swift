6d8802d93514d3511d7c80bf1b190626bc9231e9
sunflower <rebecca_0922@naver.com>
2021-11-04 10:17:21 +0900

    Feat: TagScrollView 구현

---
+++ Escaper/Escaper/Presentation/RoomList/Views/TagScrollView.swift
@@ -0,0 +1,78 @@
+//
+//  TagScrollView.swift
+//  Escaper
+//
+//  Created by shinheeRo on 2021/11/03.
+//
+
+import UIKit
+
+protocol TagScrollViewDelegate: AnyObject {
+    func tagSelected(element: Tagable)
+}
+
+class TagScrollView: UIScrollView {
+    weak var tagDelegate: TagScrollViewDelegate?
+
+    private var selectedButton: TagButton?
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 10
+        stackView.distribution = .fill
+        return stackView
+    }()
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.configureLayout()
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.configureLayout()
+    }
+
+    private func configureLayout() {
+        self.stackView.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(stackView)
+        NSLayoutConstraint.activate([
+            self.stackView.topAnchor.constraint(equalTo: self.frameLayoutGuide.topAnchor),
+            self.stackView.leadingAnchor.constraint(equalTo: self.contentLayoutGuide.leadingAnchor),
+            self.stackView.trailingAnchor.constraint(equalTo: self.contentLayoutGuide.trailingAnchor),
+            self.stackView.bottomAnchor.constraint(equalTo: self.frameLayoutGuide.bottomAnchor)
+        ])
+    }
+
+    func inject(elements: [Tagable]) {
+        elements.forEach { element in
+            let button = TagButton(element: element)
+            let width = self.calculateStringWidth(text: element.name)+20
+            button.widthAnchor.constraint(equalToConstant: width).isActive = true
+            button.addTarget(self, action: #selector(buttonTouched(sender:)), for: .touchUpInside)
+            self.stackView.addArrangedSubview(button)
+        }
+
+        if let first = self.stackView.arrangedSubviews.first as? TagButton {
+            self.selectedButton = first
+            self.selectedButton?.touched()
+        }
+    }
+
+    @objc func buttonTouched(sender: TagButton) {
+        guard self.selectedButton !== sender else { return }
+        self.selectedButton?.untouched()
+        self.selectedButton = sender
+        self.selectedButton?.touched()
+
+        guard let element = sender.element else { return }
+        self.tagDelegate?.tagSelected(element: element)
+    }
+
+    private func calculateStringWidth(text: String) -> CGFloat {
+        let temporaryLabel = UILabel()
+        temporaryLabel.text = text
+        return CGFloat(temporaryLabel.intrinsicContentSize.width)
+    }
+
+}
