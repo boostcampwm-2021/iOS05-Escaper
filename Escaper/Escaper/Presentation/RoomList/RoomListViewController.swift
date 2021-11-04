48100fba205b1e58fdf476590607a166d938fff5
sunflower <rebecca_0922@naver.com>
2021-11-04 10:17:58 +0900

    Feat: RoomListViewController 구현

--- Escaper/Escaper/Presentation/RoomList/RoomListViewController.swift
+++ Escaper/Escaper/Presentation/RoomList/RoomListViewController.swift
@@ -7,9 +7,25 @@
 
 import UIKit
 
-class RoomListViewController: UIViewController {
+class RoomListViewController: DefaultViewController {
+
+    enum Constant {
+        static let tagViewHeight = CGFloat(35)
+    }
 
     private var viewModel: RoomListViewModelInterface!
+
+    private var genreTagScrollView: TagScrollView = {
+        let tagScrollView: TagScrollView = TagScrollView()
+        tagScrollView.showsHorizontalScrollIndicator = false
+        return tagScrollView
+    }()
+
+    private var sortingOptionTagScrollView: TagScrollView = {
+        let tagScrollView: TagScrollView = TagScrollView()
+        tagScrollView.showsHorizontalScrollIndicator = false
+        return tagScrollView
+    }()
 
     func create() {
 //        let repository = DefaultNetworkService()
@@ -19,7 +35,43 @@ class RoomListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .cyan
+
+        self.configureGenreTagScrollView()
+        self.configureSortingOptionTagScrollView()
+    }
+
+    private func configureGenreTagScrollView() {
+        self.genreTagScrollView.tagDelegate = self
+        self.genreTagScrollView.inject(elements: Genre.allCases)
+
+        self.genreTagScrollView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(self.genreTagScrollView)
+        NSLayoutConstraint.activate([
+            self.genreTagScrollView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            self.genreTagScrollView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            self.genreTagScrollView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
+            self.genreTagScrollView.heightAnchor.constraint(equalToConstant: Constant.tagViewHeight)
+        ])
+    }
+
+    private func configureSortingOptionTagScrollView() {
+        self.sortingOptionTagScrollView.tagDelegate = self
+        self.sortingOptionTagScrollView.inject(elements: SortingOption.allCases)
+
+        self.sortingOptionTagScrollView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(self.sortingOptionTagScrollView)
+        NSLayoutConstraint.activate([
+            self.sortingOptionTagScrollView.topAnchor.constraint(equalTo: self.genreTagScrollView.bottomAnchor, constant: 10),
+            self.sortingOptionTagScrollView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            self.sortingOptionTagScrollView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
+            self.sortingOptionTagScrollView.heightAnchor.constraint(equalToConstant: Constant.tagViewHeight)
+        ])
+    }
+}
+
+extension RoomListViewController: TagScrollViewDelegate {
+    func tagSelected(element: Tagable) {
+        print(element.name)
     }
 
 }
