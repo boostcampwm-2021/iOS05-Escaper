db3c471e2f1b0c2d757ec1f726113fd2b34747f6
jeff <xorgus1234@gmail.com>
2021-11-05 00:18:45 +0900

    Fix: SceneDelegate 수정

--- Escaper/Escaper/Application/SceneDelegate.swift
+++ Escaper/Escaper/Application/SceneDelegate.swift
@@ -13,10 +13,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-
+        let viewController = RoomListViewController()
+        viewController.create()
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = RoomListViewController()
+        window?.rootViewController = viewController
         window?.makeKeyAndVisible()
     }
 
