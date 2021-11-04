55df6e4934fad6505e13b4e736f127ab5fd188f6
jeff <xorgus1234@gmail.com>
2021-11-04 18:29:21 +0900

    Feat: GeoLocation에 clLocation 추가

--- Escaper/Escaper/Domain/DataStruct/GeoLocation.swift
+++ Escaper/Escaper/Domain/DataStruct/GeoLocation.swift
@@ -6,17 +6,19 @@
 //
 
 import Foundation
+import CoreLocation
 
 struct GeoLocation: Codable {
     let latitude: Double
     let longitude: Double
-
     var toDictionary: [String: Any] {
         let dictionary: [String: Any] = [
             "latitude": self.latitude,
             "longitude": self.longitude
         ]
-
         return dictionary
+    }
+    var clLocation: CLLocation {
+        return CLLocation.init(latitude: self.latitude, longitude: self.longitude)
     }
 }
