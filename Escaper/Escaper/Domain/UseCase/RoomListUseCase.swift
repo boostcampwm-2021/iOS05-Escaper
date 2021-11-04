45a319a26719683261c66701bb8364c47d7076d9
jeff <xorgus1234@gmail.com>
2021-11-04 18:46:53 +0900

    Feat: RoomListUseCase 구현

--- Escaper/Escaper/Domain/UseCase/RoomListUseCase.swift
+++ Escaper/Escaper/Domain/UseCase/RoomListUseCase.swift
@@ -6,14 +6,38 @@
 //
 
 import Foundation
+import CoreLocation
 
 protocol RoomListUseCaseInterface {
     func fetch(genre: Genre, completion: @escaping (Result<[Room], Error>) -> Void)
 }
 
 class DefaultRoomListUseCase: RoomListUseCaseInterface {
-    func fetch(genre: Genre, completion: @escaping (Result<[Room], Error>) -> Void) {
+    let repository: RoomListRepository
 
+    init(repository: RoomListRepository) {
+        self.repository = repository
     }
 
+    func fetch(genre: Genre, completion: @escaping (Result<[Room], Error>) -> Void) {
+        self.fetchCurrentDistrict(genre: genre, completion: completion)
+    }
+
+    private func fetchCurrentDistrict(genre: Genre, completion: @escaping (Result<[Room], Error>) -> Void) {
+        let locationManager = CLLocationManager()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+
+        guard let location = locationManager.location else { return }
+        let geocoder = CLGeocoder()
+        let locale = Locale(identifier: "Ko-kr")
+        geocoder.reverseGeocodeLocation(location, preferredLocale: locale) {  placeMarks, _ in
+            guard let address: [CLPlacemark] = placeMarks,
+                  let locality = address.last?.locality,
+                  let district = District.init(rawValue: locality) else { return }
+
+            self.repository.get(genre: genre, district: district, completion: completion)
+        }
+
+    }
 }
