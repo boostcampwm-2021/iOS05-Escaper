484b552b5337abe86ca03cd8251832478604de36
wansikChoi <wansook0316@gmail.com>
2021-11-09 15:21:01 +0900

    Feat: ViewController 원형 추가

---
+++ Escaper/Escaper/Presentation/LeaderBoard/LeaderBoardViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LeaderBoardViewController.swift
+//  Escaper
+//
+//  Created by 최완식 on 2021/11/09.
+//
+
+import UIKit
+
+class LeaderBoardViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    func create() {
+        // 의존성 주입
+    }
+}
---
+++ Escaper/Escaper/Presentation/Record/RecordViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RecordViewController.swift
+//  Escaper
+//
+//  Created by 최완식 on 2021/11/09.
+//
+
+import UIKit
+
+class RecordViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    func create() {
+        // 외부 의존성 주입
+    }
+}
