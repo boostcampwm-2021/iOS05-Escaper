1fd2013546bb3605b79c60cf336212f5afe01e09
wansikChoi <wansook0316@gmail.com>
2021-11-09 21:37:41 +0900

    Fix: 디자인 시스템 font 사용법 변경

--- Escaper/Escaper/Common/Library/EDSKit.swift
+++ Escaper/Escaper/Common/Library/EDSKit.swift
@@ -9,60 +9,61 @@ import UIKit
 
 typealias EDSColor = EDSKit.Color
 typealias EDSLabel = EDSKit.Label
+typealias EDSImage = EDSKit.Image
 
 enum EDSKit {
     enum Label {
         static func h01B(text: String = "", color: Color) -> UILabel {
             let label = Label.makeLabel(text: text, color: color)
-            label.font = UIFont.boldSystemFont(ofSize: 24)
+            label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
             return label
         }
 
         static func h02B(text: String = "", color: Color) -> UILabel {
             let label = Label.makeLabel(text: text, color: color)
-            label.font = UIFont.boldSystemFont(ofSize: 22)
+            label.font = UIFont.systemFont(ofSize: 22, weight: .bold)
             return label
         }
 
         static func h03B(text: String = "", color: Color) -> UILabel {
             let label = Label.makeLabel(text: text, color: color)
-            label.font = UIFont.boldSystemFont(ofSize: 20)
+            label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
             return label
         }
 
         static func b01B(text: String = "", color: Color) -> UILabel {
             let label = Label.makeLabel(text: text, color: color)
-            label.font = UIFont.boldSystemFont(ofSize: 14)
+            label.font = UIFont.systemFont(ofSize: 14, weight: .bold)
             return label
         }
 
         static func b02B(text: String = "", color: Color) -> UILabel {
             let label = Label.makeLabel(text: text, color: color)
-            label.font = UIFont.boldSystemFont(ofSize: 12)
+            label.font = UIFont.systemFont(ofSize: 12, weight: .bold)
             return label
         }
 
         static func b03B(text: String = "", color: Color) -> UILabel {
             let label = Label.makeLabel(text: text, color: color)
-            label.font = UIFont.boldSystemFont(ofSize: 10)
+            label.font = UIFont.systemFont(ofSize: 10, weight: .bold)
             return label
         }
 
         static func b01R(text: String = "", color: Color) -> UILabel {
             let label = Label.makeLabel(text: text, color: color)
-            label.font = UIFont.systemFont(ofSize: 14)
+            label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
             return label
         }
 
         static func b02R(text: String = "", color: Color) -> UILabel {
             let label = Label.makeLabel(text: text, color: color)
-            label.font = UIFont.systemFont(ofSize: 12)
+            label.font = UIFont.systemFont(ofSize: 12, weight: .regular)
             return label
         }
 
         static func b03R(text: String = "", color: Color) -> UILabel {
             let label = Label.makeLabel(text: text, color: color)
-            label.font = UIFont.systemFont(ofSize: 10)
+            label.font = UIFont.systemFont(ofSize: 10, weight: .regular)
             return label
         }
     }
@@ -84,6 +85,14 @@ enum EDSKit {
         var value: UIColor? {
             return UIColor(named: String(describing: self))
         }
+    }
+
+    enum Image {
+        var value: UIImage? {
+            return UIImage(named: String(describing: self))
+        }
+        
+        case chevronDown
     }
 }
 
