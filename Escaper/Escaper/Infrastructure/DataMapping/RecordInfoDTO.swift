41032ff2922840f76e719601e10a2744322198db
wansikChoi <wansook0316@gmail.com>
2021-11-11 15:21:19 +0900

    Feat: Record Info DTO 추가
    - Record Info 객체를 Firebase에 올리고 받기 위한 전송 객체

---
+++ Escaper/Escaper/Infrastructure/DataMapping/RecordInfoDTO.swift
@@ -0,0 +1,36 @@
+//
+//  RecordInfoDTO.swift
+//  Escaper
+//
+//  Created by 최완식 on 2021/11/10.
+//
+
+import Foundation
+
+
+struct RecordInfoDTO: Codable {
+    var userEmail: String
+    var roomId: String
+    var satisfaction: Double
+    var isSuccess: Bool
+    var time: Int
+
+    func toDictionary() -> [String: Any] {
+        let dictionary: [String: Any] = [
+            "userEmail": self.userEmail,
+            "roomId": self.roomId,
+            "satisfaction": self.satisfaction,
+            "isSuccess": self.isSuccess,
+            "time": self.time
+        ]
+        return dictionary
+    }
+
+    func toDomain() -> RecordInfo {
+        return RecordInfo(userEmail: self.userEmail,
+                                roomId: self.roomId,
+                                satisfaction: Rating(rawValue: Int(self.satisfaction.rounded())) ?? Rating.zero,
+                                isSuccess: self.isSuccess,
+                                time: self.time)
+    }
+}
