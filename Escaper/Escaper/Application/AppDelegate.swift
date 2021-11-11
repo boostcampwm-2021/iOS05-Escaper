41d3f90b5a5104c474d4a87f3d4f00b7ea6e166b
wansikChoi <wansook0316@gmail.com>
2021-11-11 22:40:53 +0900

    Feat: Firebase configure 이동

--- Escaper/Escaper/Application/AppDelegate.swift
+++ Escaper/Escaper/Application/AppDelegate.swift
@@ -6,11 +6,13 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
         return true
     }
 
