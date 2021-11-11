f740a0c7610c37f02ae8ccd4a5662bf3c47cdd93
sunflower <rebecca_0922@naver.com>
2021-11-11 23:18:10 +0900

    Feat: RecordViewController 레이아웃 추가 작업 & 하단부와 연결

--- Escaper/Escaper/Presentation/Record/RecordViewController.swift
+++ Escaper/Escaper/Presentation/Record/RecordViewController.swift
@@ -14,7 +14,7 @@ class RecordViewController: DefaultViewController {
         static let defaultVerticalSpace = deviceHeight * 0.015
         static let collectionViewHeight = deviceHeight * 0.55
         static let buttonHeight = deviceHeight * 0.06
-        static let longHorizontalSpace = CGFloat(160)
+        static let longHorizontalSpace = CGFloat(120)
         static let shortHorizontalSpace = CGFloat(60)
     }
 
@@ -29,14 +29,18 @@ class RecordViewController: DefaultViewController {
         case level3 = "이정도면, 왠만한 방은 거의 탈출해보셨겠는걸요?"
     }
 
-    private enum Section {
-        case card
+    private enum Section: Int {
+        var index: Int {
+            return self.rawValue
+        }
+
+        case card = 0
     }
 
-    typealias Datasource = UICollectionViewDiffableDataSource<Section, Record>
+    private typealias Datasource = UICollectionViewDiffableDataSource<Section, Record>
 
-    private let viewModel = RecordViewModel?
-    private var datasource = Datasource?
+    private var viewModel: RecordViewModel?
+    private var datasource: Datasource?
     private let titleLabel: UILabel = {
         let label: UILabel = EDSLabel.h01B(text: "탈출 기록", color: .skullWhite)
         label.textAlignment = .center
@@ -51,16 +55,20 @@ class RecordViewController: DefaultViewController {
         return label
     }()
     private let greetingLabel: UILabel = {
-        let label: UILabel = EDSLabel.b02B(text: GreetingMessage.level1.name, color: .skullWhite)
+        let label: UILabel = EDSLabel.b02B(text: GreetingMessage.level1.value, color: .skullWhite)
         label.textAlignment = .center
         return label
     }()
     private let recordCollectionView: UICollectionView = {
         let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
-        layout.minimumLineSpacing = 10
         layout.scrollDirection = .horizontal
+        layout.estimatedItemSize = .zero
         let collectionView: UICollectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.contentInsetAdjustmentBehavior = .never
         collectionView.backgroundColor = EDSColor.bloodyBlack.value
+        collectionView.contentInset = UIEdgeInsets(top: 0, left: UIScreen.main.bounds.size.width*0.1, bottom: 0, right: UIScreen.main.bounds.size.width*0.1)
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.decelerationRate = .fast
         return collectionView
     }()
     private let addButton: UIButton = {
@@ -77,29 +85,94 @@ class RecordViewController: DefaultViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         create()
-        bindViewModel()
         configure()
         configureLayout()
+        bindViewModel()
     }
 
     func create() {
-        let repository = RecordRepository(service: FirebaseService.shared)
-        let usecase = RecordUseCase(repository: repository)
-        let viewModel = DefaultRecordViewModel(usecase: usecase)
+        let roomRepository = RoomListRepository(service: FirebaseService.shared)
+        let recordRepository = RecordRepository(service: FirebaseService.shared)
+        let usecase = RecordUsecase(roomRepository: roomRepository, recordRepository: recordRepository)
+        let viewModel = DefaultRecordViewModel(useCase: usecase)
         self.viewModel = viewModel
     }
 
     func bindViewModel() {
-        self.viewModel.records.bind { records in
-            self.configureRecordCollectionViewData(records: records)
+        self.viewModel?.records.observe(on: self) { [weak self] result in
+            self?.configureRecordCollectionViewData(records: result)
         }
+        self.viewModel?.fetch(userEmail: "lamar.schaden@hotmail.com")
+    }
+}
+
+extension RecordViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return CGFloat(10)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: UIScreen.main.bounds.size.width*0.8, height: Constant.deviceHeight*0.55)
+    }
+
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        let cellWidthIncludingSpacing = UIScreen.main.bounds.size.width*0.8 + CGFloat(10)
+        var offset = targetContentOffset.pointee
+        let index = (offset.x + scrollView.contentInset.left) / cellWidthIncludingSpacing
+        let roundedIndex = round(index)
+        offset = CGPoint(x: roundedIndex * cellWidthIncludingSpacing - scrollView.contentInset.left, y: scrollView.contentInset.top)
+        targetContentOffset.pointee = offset
+    }
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let cellWidthIncludingSpacing = UIScreen.main.bounds.size.width*0.8 + CGFloat(10)
+        let offsetX = self.recordCollectionView.contentOffset.x
+        let index = (offsetX + self.recordCollectionView.contentInset.left) / cellWidthIncludingSpacing
+        let roundedIndex = round(index)
+        let indexPath = IndexPath(item: Int(roundedIndex), section: Section.card.index)
+        if let cell = self.recordCollectionView.cellForItem(at: indexPath) {
+            animateZoomforCell(zoomCell: cell)
+        }
+        if Int(roundedIndex) != 0 {
+            let index = Int(roundedIndex) - 1
+            let indexPath = IndexPath(item: index, section: Section.card.index)
+            if let previousCell = self.recordCollectionView.cellForItem(at: indexPath) {
+                animateZoomforCellremove(zoomCell: previousCell)
+            }
+        }
+        if Int(roundedIndex) != self.recordCollectionView.numberOfItems(inSection: Section.card.index)-1 {
+            let index = Int(roundedIndex) + 1
+            let indexPath = IndexPath(item: index, section: Section.card.index)
+            if let nextCell = self.recordCollectionView.cellForItem(at: indexPath) {
+                animateZoomforCellremove(zoomCell: nextCell)
+            }
+        }
+    }
+
+    func animateZoomforCell(zoomCell: UICollectionViewCell) {
+        UIView.animate(withDuration: 0.2,
+                        delay: 0,
+                        options: .curveEaseOut,
+                        animations: {
+                            zoomCell.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+                        },
+                        completion: nil)
+    }
+
+    func animateZoomforCellremove(zoomCell: UICollectionViewCell) {
+        UIView.animate(withDuration: 0,
+                       delay: 0,
+                       options: .curveEaseOut,
+                       animations: {
+                            zoomCell.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
+                       },
+                       completion: nil)
     }
 }
 
 extension RecordViewController {
     func configure() {
         configureRecordCollectionView()
-        configureRecordCollectionViewData(records: [Record])
     }
 
     func configureLayout() {
@@ -111,12 +184,13 @@ extension RecordViewController {
     }
 
     func configureRecordCollectionView() {
-        self.datasource = Datasource(collectionView: self.recordCollectionView) {
-            (collectionView, indexPath, itemIdentifier) -> UICollectionViewCell in
-            guard let cell = self.recordCollectionView.dequeueConfiguredReusableCell(using: RecordCollectionViewCell.identifier, for: indexPath) as? RecordCollectionViewCell else { return UICollectionViewCell () }
+        self.recordCollectionView.delegate = self
+        self.datasource = Datasource(collectionView: self.recordCollectionView) { (collectionView, indexPath, itemIdentifier) -> UICollectionViewCell in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RecordCollectionViewCell.identifier, for: indexPath) as? RecordCollectionViewCell else { return UICollectionViewCell() }
             cell.update(record: itemIdentifier)
             return cell
         }
+        self.recordCollectionView.register(RecordCollectionViewCell.self, forCellWithReuseIdentifier: RecordCollectionViewCell.identifier)
     }
 
     func configureRecordCollectionViewData(records: [Record]) {
@@ -124,6 +198,12 @@ extension RecordViewController {
         snapshot.appendSections([Section.card])
         snapshot.appendItems(records, toSection: Section.card)
         self.datasource?.apply(snapshot, animatingDifferences: false)
+        if records.count > 1 {
+            let indexPath = IndexPath(item: 1, section: Section.card.index)
+            if let cell = self.recordCollectionView.cellForItem(at: indexPath) {
+                animateZoomforCellremove(zoomCell: cell)
+            }
+        }
         configureRecordSummary(count: records.count)
     }
 
