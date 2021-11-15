f2cbb20f74f056486b5d28ec9e5f29e3221980bd
jeff <xorgus1234@gmail.com>
2021-11-15 16:09:54 +0900

    Fix: 리뷰 반영

--- Escaper/Escaper/Common/Extensions/UIView+RoundCorners.swift
+++ Escaper/Escaper/Common/Extensions/UIView+RoundCorners.swift
@@ -9,9 +9,9 @@ import UIKit
 
 extension UIView {
     func roundCorners(corners: UIRectCorner, radius: CGFloat) {
-        let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
         let mask = CAShapeLayer()
         mask.path = path.cgPath
-        layer.mask = mask
+        self.layer.mask = mask
     }
 }
