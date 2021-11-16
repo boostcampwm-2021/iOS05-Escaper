0483cfe5c8d0e59e2ff61061b00d31d8f34076da
yepark <poisonF2@naver.com>
2021-11-16 11:16:53 +0900

    Feat: LeaderBoard/configureScrollViewLayout

--- Escaper/Escaper/Presentation/LeaderBoard/LeaderBoardViewController.swift
+++ Escaper/Escaper/Presentation/LeaderBoard/LeaderBoardViewController.swift
@@ -8,11 +8,31 @@
 import UIKit
 
 class LeaderBoardViewController: UIViewController {
+    private let scrollView = UIScrollView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configureLayout()
     }
 
     func create() {
         // 의존성 주입
+    }
+}
+
+private extension LeaderBoardViewController {
+    func configureLayout() {
+        configureScrollViewLayout()
+    }
+
+    func configureScrollViewLayout() {
+        self.scrollView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(self.scrollView)
+        NSLayoutConstraint.activate([
+            self.scrollView.frameLayoutGuide.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            self.scrollView.frameLayoutGuide.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            self.scrollView.frameLayoutGuide.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
+            self.scrollView.frameLayoutGuide.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
 }
