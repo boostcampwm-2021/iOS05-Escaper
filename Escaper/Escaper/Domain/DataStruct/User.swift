39706ac9d5f0ad4134f5e917e4c0bdf9c804f54a
yepark <poisonF2@naver.com>
2021-11-17 21:18:27 +0900

    Feat: User DataStruct 구조체 생성

---
+++ Escaper/Escaper/Domain/DataStruct/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  Escaper
+//
+//  Created by 박영광 on 2021/11/17.
+//
+
+import Foundation
+
+struct User {
+    let email: String
+    let name: String
+    let password: String
+    let imageURL: URL?
+    let score: Int
+}
