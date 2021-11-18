1b529a1990a7a45231bcd4081fa365c8bfa82d2b
sunflower <rebecca_0922@naver.com>
2021-11-18 13:37:09 +0900

    Feat: ViewType enum 생성

---
+++ Escaper/Escaper/Common/Library/ViewType.swift
@@ -0,0 +1,14 @@
+//
+//  ViewType.swift
+//  Escaper
+//
+//  Created by shinheeRo on 2021/11/16.
+//
+
+import Foundation
+
+enum ViewType {
+    case email
+    case password
+    case passwordCheck
+}
