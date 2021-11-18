295bb3a6defab4051914943480b05cc93b604e9a
wansikChoi <wansook0316@gmail.com>
2021-11-18 18:01:04 +0900

    Feat: Helper function, Array Extension (isNotEmpty) 추가

--- Escaper/Escaper/Common/Library/Helper.swift
+++ Escaper/Escaper/Common/Library/Helper.swift
@@ -9,6 +9,21 @@ import Foundation
 
 enum Helper {
     static func parseUsername(email: String) -> String? {
-        return email.components(separatedBy: "@").first 
+        return email.components(separatedBy: "@").first
+    }
+
+    static func binarySearch(value: Int, sortedValues: [Int]) -> Int? {
+        var left = 0, right = sortedValues.count-1
+        while left <= right {
+            let mid = Int((left + right)/2)
+            if sortedValues[mid] == value {
+                return mid + 1
+            } else if sortedValues[mid] > value {
+                right = mid - 1
+            } else if sortedValues[mid] < value {
+                left = mid + 1
+            }
+        }
+        return nil
     }
 }
