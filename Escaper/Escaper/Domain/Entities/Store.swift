79d002b3ebbdbf59573c372d28a05af4cd829656
wansikChoi <wansook0316@gmail.com>
2021-11-18 17:50:49 +0900

    Feat: Store, StoreDTO 추가

---
+++ Escaper/Escaper/Domain/Entities/Store.swift
@@ -0,0 +1,19 @@
+//
+//  Store.swift
+//  Escaper
+//
+//  Created by 최완식 on 2021/11/17.
+//
+
+import Foundation
+import CoreLocation
+
+struct Store {
+    var name: String
+    var homePage: String
+    var telephone: String
+    var address: String
+    var geoLocation: CLLocation
+    var district: District
+    var roomIds: [String]
+}
