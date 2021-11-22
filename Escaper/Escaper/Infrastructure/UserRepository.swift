d967d85a04dc560c4e56a7bfea4984b4bc51fc14
sunflower <rebecca_0922@naver.com>
2021-11-22 22:03:38 +0900

    Feat: User Repository 구현

---
+++ Escaper/Escaper/Infrastructure/UserRepository.swift
@@ -0,0 +1,52 @@
+//
+//  UserRepository.swift
+//  Escaper
+//
+//  Created by shinheeRo on 2021/11/22.
+//
+
+import Foundation
+
+final class UserRepository: UserRepositoryInterface {
+    private let service: UserNetwork
+
+    init(service: UserNetwork) {
+        self.service = service
+    }
+
+    func query(userEmail: String, completion: @escaping (Result<Bool, Error>) -> Void) {
+        self.service.queryUser(email: userEmail) { result in
+            switch result {
+            case .success(let isOverlaped):
+                if isOverlaped {
+                    completion(Result.success(true))
+                }
+                else {
+                    completion(Result.success(false))
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+
+    func confirm(userEmail: String, userPassword: String, completion: @escaping (Result<Bool, Error>) -> Void) {
+        self.service.confirmUser(email: userEmail, password: userPassword) { result in
+            switch result {
+            case .success(let isOverlaped):
+                if isOverlaped {
+                    completion(Result.success(true))
+                }
+                else {
+                    completion(Result.success(false))
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+
+    func add(user: User) {
+        self.service.addUser(user: user)
+    }
+}
