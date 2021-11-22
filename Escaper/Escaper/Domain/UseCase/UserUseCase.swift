c8c266bca6dff43c1807d376914cba2f16702e96
sunflower <rebecca_0922@naver.com>
2021-11-22 22:03:53 +0900

    Feat: User UseCase 구현

---
+++ Escaper/Escaper/Domain/UseCase/UserUseCase.swift
@@ -0,0 +1,52 @@
+//
+//  UserUseCase.swift
+//  Escaper
+//
+//  Created by shinheeRo on 2021/11/22.
+//
+
+import Foundation
+
+final class UserUseCase {
+    private let repository: UserRepositoryInterface
+
+    init(userRepository: UserRepositoryInterface) {
+        self.repository = userRepository
+    }
+
+    func query(userEmail: String, completion: @escaping (Result<Bool, Error>) -> Void) {
+        self.repository.query(userEmail: userEmail) { result in
+            switch result {
+            case .success(let isOverlaped):
+                if isOverlaped {
+                    completion(Result.success(true))
+                }
+                else {
+                    completion(Result.success(false))
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+
+    func confirm(userEmail: String, userPassword: String, completion: @escaping (Result<Bool, Error>) -> Void) {
+        self.repository.confirm(userEmail: userEmail, userPassword: userPassword) { result in
+            switch result {
+            case .success(let isOverlaped):
+                if isOverlaped {
+                    completion(Result.success(true))
+                }
+                else {
+                    completion(Result.success(false))
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+
+    func add(user: User) {
+        self.repository.add(user: user)
+    }
+}
