834f8863bcf72d934e5df386bea5ee587f67a93f
wansikChoi <wansook0316@gmail.com>
2021-11-22 21:22:26 +0900

    Feat: UserSupervisor 구현

---
+++ Escaper/Escaper/Infrastructure/UserSupervisor.swift
@@ -0,0 +1,28 @@
+//
+//  UserSupervisor.swift
+//  Escaper
+//
+//  Created by 최완식 on 2021/11/22.
+//
+
+import Foundation
+
+class UserSupervisor {
+    static let shared = UserSupervisor()
+
+    private var email: String = ""
+
+    init() {}
+
+    var isLogined: Bool {
+        return self.email.isEmpty
+    }
+
+    func login(email: String) {
+        self.email = email
+    }
+
+    func logout() {
+        self.email = ""
+    }
+}
