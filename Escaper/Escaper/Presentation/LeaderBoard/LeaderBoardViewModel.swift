0ceef29dca7e8aae5bc7e779a8861efea4480863
yepark <poisonF2@naver.com>
2021-11-22 17:29:48 +0900

    Feat: LeaderBoard 뷰모델 기능 추가

--- Escaper/Escaper/Presentation/LeaderBoard/LeaderBoardViewModel.swift
+++ Escaper/Escaper/Presentation/LeaderBoard/LeaderBoardViewModel.swift
@@ -9,11 +9,24 @@ import Foundation
 
 protocol LeaderBoardViewModelInterface {
     var users: [User] { get }
+    var topThreeUser: [User] { get }
+    var topTenUser: [User] { get }
+    var count: Int { get }
+    func fetch()
 }
 
 final class DefaultLeadeBoardViewModel: LeaderBoardViewModelInterface {
     private let usecase: LeaderBoardUseCaseInterface
     private(set) var users: [User]
+    var topThreeUser: [User] {
+        return self.users.prefix(3).map {$0}
+    }
+    var topTenUser: [User] {
+        return self.users.prefix(10).map {$0}
+    }
+    var count: Int {
+        return self.users.count
+    }
 
     init(usecase: LeaderBoardUseCaseInterface) {
         self.usecase = usecase
@@ -25,8 +38,6 @@ final class DefaultLeadeBoardViewModel: LeaderBoardViewModelInterface {
             switch result {
             case .success(let users):
                 self.users = users.sorted {$0.score < $1.score}
-                .prefix(10)
-                .map {$0}
             case .failure(let error):
                 print(error)
             }
