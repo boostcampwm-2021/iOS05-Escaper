d51305d0b1a32a7722e94834dafe0c281ddd74ff
yepark <poisonF2@naver.com>
2021-11-25 18:21:37 +0900

    Feat: roomDetailView UI 1차 완료

--- Escaper/Escaper/Presentation/RoomDetail/Views/RoomDetailInfoView.swift
+++ Escaper/Escaper/Presentation/RoomDetail/Views/RoomDetailInfoView.swift
@@ -8,20 +8,8 @@
 import UIKit
 
 final class RoomDetailInfoView: UIView {
-    private let levelLabel = EDSLabel.b01B(text: "난이도", color: .skullWhite)
-    private let levelRatingView = RatingView()
-    private let satisfactionLabel = EDSLabel.b01B(text: "만족도", color: .skullLightWhite)
-    private let satisfactionRatingView = RatingView()
-    private let genreTitleLabel = EDSLabel.b01B(text: "장르", color: .skullLightWhite)
-    private let genreLabel = EDSLabel.b02R(text: "장르가 없습니다.", color: .skullLightWhite)
-    private let homepageTitleLabel = EDSLabel.b01B(text: "홈페이지", color: .skullLightWhite)
-    private let homepageLabel: UILabel = {
-        let label = EDSLabel.b02R(text: "홈페이지가 없습니다.", color: .skullLightWhite)
-        label.lineBreakMode = .byTruncatingTail
-        return label
-    }()
-    private let phoneNumberTitleLabel = EDSLabel.b01B(text: "전화번호", color: .skullLightWhite)
-    private let phoneNumberLabel = EDSLabel.b02R(text: "전화번호가 없습니다.", color: .skullLightWhite)
+    private let leftInfoView = InfoDescriptionStackView()
+    private let rightInfoView = InfoDescriptionStackView()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -34,129 +22,35 @@ final class RoomDetailInfoView: UIView {
     }
 
     func update(room: Room) {
-        self.levelRatingView.fill(rating: Rating(rawValue: room.difficulty)!)
-        self.satisfactionRatingView.fill(rating: Rating(rawValue: Int(room.averageSatisfaction))!)
-        self.genreLabel.text = room.genre.name
+        self.leftInfoView.inject(view: InfoDescriptionDetailStackView(title: "장르", content: room.genre.name))
+        self.leftInfoView.inject(view: InfoDescriptionDetailStackView(title: "활동성", content: room.activity))
+        self.rightInfoView.inject(view: InfoDescriptionDetailStackView(title: "제한 시간", content: "\(room.timeLimit)분"))
+        self.rightInfoView.inject(view: InfoDescriptionDetailStackView(title: "최대 인원", content: "\(room.maxParty)인"))
     }
 }
 
 private extension RoomDetailInfoView {
-    enum Constant {
-        static let titleLabelSize = CGFloat(150)
-        static let ratingViewHeight = CGFloat(12)
-        static let ratingViewWidth = CGFloat(72)
-        static let ratingViewAnchorY = CGFloat(1)
-        static let sideSpace = CGFloat(16)
-        static let gapSapce = CGFloat(48)
-    }
-
     func configureLayout() {
-        self.configureLevelLabelLayout()
-        self.configureSatisfactionLabelLayout()
-        self.configureGenreTitleLabelLayout()
-        self.configureHomepageTitleLabelLayout()
-        self.configurePhoneNumberTitleLabelLayout()
-        self.configureLevelRatingViewLayout()
-        self.configureSatisfactionRatingViewLayout()
-        self.configureGenreLabelLayout()
-        self.configureHomepageLabelLayout()
-        self.configurePhoneNumberLabelLayout()
+        self.configureLeftInfoViewLayout()
+        self.configureRightInfoViewLayout()
     }
 
-    func configureLevelLabelLayout() {
-        self.levelLabel.translatesAutoresizingMaskIntoConstraints = false
-        self.addSubview(self.levelLabel)
+    func configureLeftInfoViewLayout() {
+        self.leftInfoView.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(self.leftInfoView)
         NSLayoutConstraint.activate([
-            self.levelLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: Constant.sideSpace),
-            self.levelLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: Constant.sideSpace)
+            self.leftInfoView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            self.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.5)
         ])
     }
 
-    func configureSatisfactionLabelLayout() {
-        self.satisfactionLabel.translatesAutoresizingMaskIntoConstraints = false
-        self.addSubview(self.satisfactionLabel)
+    func configureRightInfoViewLayout() {
+        self.rightInfoView.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(self.rightInfoView)
         NSLayoutConstraint.activate([
-            self.satisfactionLabel.leadingAnchor.constraint(equalTo: self.levelLabel.leadingAnchor),
-            self.satisfactionLabel.topAnchor.constraint(equalTo: self.levelLabel.bottomAnchor, constant: Constant.sideSpace)
-        ])
-    }
-
-    func configureGenreTitleLabelLayout() {
-        self.genreTitleLabel.translatesAutoresizingMaskIntoConstraints = false
-        self.addSubview(self.genreTitleLabel)
-        NSLayoutConstraint.activate([
-            self.genreTitleLabel.leadingAnchor.constraint(equalTo: self.satisfactionLabel.leadingAnchor),
-            self.genreTitleLabel.topAnchor.constraint(equalTo: self.satisfactionLabel.bottomAnchor, constant: Constant.sideSpace)
-        ])
-    }
-
-    func configureHomepageTitleLabelLayout() {
-        self.homepageTitleLabel.translatesAutoresizingMaskIntoConstraints = false
-        self.addSubview(self.homepageTitleLabel)
-        NSLayoutConstraint.activate([
-            self.homepageTitleLabel.leadingAnchor.constraint(equalTo: self.genreTitleLabel.leadingAnchor),
-            self.homepageTitleLabel.topAnchor.constraint(equalTo: self.genreTitleLabel.bottomAnchor, constant: Constant.sideSpace)
-        ])
-    }
-
-    func configurePhoneNumberTitleLabelLayout() {
-        self.phoneNumberTitleLabel.translatesAutoresizingMaskIntoConstraints = false
-        self.addSubview(self.phoneNumberTitleLabel)
-        NSLayoutConstraint.activate([
-            self.phoneNumberTitleLabel.leadingAnchor.constraint(equalTo: self.homepageTitleLabel.leadingAnchor),
-            self.phoneNumberTitleLabel.topAnchor.constraint(equalTo: self.homepageTitleLabel.bottomAnchor, constant: Constant.sideSpace)
-        ])
-    }
-
-    func configureLevelRatingViewLayout() {
-        self.levelRatingView.translatesAutoresizingMaskIntoConstraints = false
-        self.addSubview(self.levelRatingView)
-        NSLayoutConstraint.activate([
-            self.levelRatingView.centerYAnchor.constraint(equalTo: self.levelLabel.centerYAnchor, constant: -Constant.ratingViewAnchorY),
-            self.levelRatingView.leadingAnchor.constraint(equalTo: levelLabel.trailingAnchor, constant: Constant.gapSapce),
-            self.levelRatingView.widthAnchor.constraint(equalToConstant: Constant.ratingViewWidth),
-            self.levelRatingView.heightAnchor.constraint(equalToConstant: Constant.ratingViewHeight)
-        ])
-    }
-
-    func configureSatisfactionRatingViewLayout() {
-        self.satisfactionRatingView.translatesAutoresizingMaskIntoConstraints = false
-        self.addSubview(self.satisfactionRatingView)
-        NSLayoutConstraint.activate([
-            self.satisfactionRatingView.centerYAnchor.constraint(equalTo: self.satisfactionLabel.centerYAnchor, constant: Constant.ratingViewAnchorY),
-            self.satisfactionRatingView.leadingAnchor.constraint(equalTo: self.levelRatingView.leadingAnchor),
-            self.satisfactionRatingView.widthAnchor.constraint(equalToConstant: Constant.ratingViewWidth),
-            self.satisfactionRatingView.heightAnchor.constraint(equalToConstant: Constant.ratingViewHeight)
-        ])
-    }
-
-    func configureGenreLabelLayout() {
-        self.genreLabel.translatesAutoresizingMaskIntoConstraints = false
-        self.addSubview(self.genreLabel)
-        NSLayoutConstraint.activate([
-            self.genreLabel.centerYAnchor.constraint(equalTo: self.genreTitleLabel.centerYAnchor),
-            self.genreLabel.leadingAnchor.constraint(equalTo: self.satisfactionRatingView.leadingAnchor),
-            self.genreLabel.widthAnchor.constraint(equalToConstant: Constant.titleLabelSize)
-        ])
-    }
-
-    func configureHomepageLabelLayout() {
-        self.homepageLabel.translatesAutoresizingMaskIntoConstraints = false
-        self.addSubview(self.homepageLabel)
-        NSLayoutConstraint.activate([
-            self.homepageLabel.centerYAnchor.constraint(equalTo: self.homepageTitleLabel.centerYAnchor),
-            self.homepageLabel.leadingAnchor.constraint(equalTo: self.genreLabel.leadingAnchor),
-            self.homepageLabel.widthAnchor.constraint(equalToConstant: Constant.titleLabelSize)
-        ])
-    }
-
-    func configurePhoneNumberLabelLayout() {
-        self.phoneNumberLabel.translatesAutoresizingMaskIntoConstraints = false
-        self.addSubview(self.phoneNumberLabel)
-        NSLayoutConstraint.activate([
-            self.phoneNumberLabel.centerYAnchor.constraint(equalTo: self.phoneNumberTitleLabel.centerYAnchor),
-            self.phoneNumberLabel.leadingAnchor.constraint(equalTo: self.homepageLabel.leadingAnchor),
-            self.phoneNumberLabel.widthAnchor.constraint(equalToConstant: Constant.titleLabelSize)
+            self.rightInfoView.leadingAnchor.constraint(equalTo: self.leftInfoView.trailingAnchor),
+            self.rightInfoView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.5),
+            self.rightInfoView.trailingAnchor.constraint(equalTo: self.trailingAnchor)
         ])
     }
 }
