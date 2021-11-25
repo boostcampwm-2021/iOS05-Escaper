c4e86d99ded6dcfbd6c7e50c6b35d302a32ae7f4
yepark <poisonF2@naver.com>
2021-11-25 17:39:40 +0900

    Feat: roomDetail Description 추가

--- Escaper/Escaper/Presentation/RoomDetail/Views/RoomDetailInfoView.swift
+++ Escaper/Escaper/Presentation/RoomDetail/Views/RoomDetailInfoView.swift
@@ -37,9 +37,6 @@ final class RoomDetailInfoView: UIView {
         self.levelRatingView.fill(rating: Rating(rawValue: room.difficulty)!)
         self.satisfactionRatingView.fill(rating: Rating(rawValue: Int(room.averageSatisfaction))!)
         self.genreLabel.text = room.genre.name
-        // TODO: - StoreUsecase 추가하여 Store 정보를 가져와서 반영해야 함
-//        self.homepageLabel.text = room.homepage
-//        self.phoneNumberLabel.text = room.telephone
     }
 }
 
