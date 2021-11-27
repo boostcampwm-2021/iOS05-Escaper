a822f563d91c8971318b5b7f4dfe93ddb238f1dd
sunflower <rebecca_0922@naver.com>
2021-11-27 16:48:47 +0900

    Feat: Validator 내부의 함수들이 string이 아니라 bool타입을 반환하도록 개선

--- Escaper/Escaper/Common/Library/Validator.swift
+++ Escaper/Escaper/Common/Library/Validator.swift
@@ -8,52 +8,31 @@
 import Foundation
 
 enum Validator {
-    enum State: String {
-        case normal = ""
-        case emailFormatError = "이메일 형식이 올바르지 않습니다."
-        case numberOfDigitsError = "8자리 이상 입력해주세요."
-        case discordanceError = "비밀번호와 일치하지 않습니다."
-        case alreadyExistError = "이미 등록된 이메일입니다."
-        case notConfirmedError = "아이디와 비밀번호를 다시 확인해주세요."
-
-        var value: String {
-            return self.rawValue
-        }
-    }
-
-    static var notConfirmedErrorString: String {
-        return State.notConfirmedError.value
-    }
-
-    static var alreadyExistErrorString: String {
-        return State.alreadyExistError.value
-    }
-
-    static func checkEmailFormat(text: String) -> String {
+    static func checkEmailFormat(text: String) -> Bool {
         let range = NSRange(location: 0, length: text.count)
         let pattern = "^[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}$"
         if let idRegex = try? NSRegularExpression(pattern: pattern) {
             let isMatched = idRegex.matches(in: text, options: [], range: range)
             if isMatched == [] {
-                return State.emailFormatError.value
+                return false
             } else {
-                return ""
+                return true
             }
         }
-        return ""
+        return true
     }
 
-    static func checkNumberOfDigits(text: String) -> String {
+    static func checkNumberOfDigits(text: String) -> Bool {
         if text.count < 8 {
-            return State.numberOfDigitsError.value
+            return false
         }
-        return State.normal.value
+        return true
     }
 
-    static func checkDiscordance(text1: String, text2: String) -> String {
+    static func checkDiscordance(text1: String, text2: String) -> Bool {
         if text1 != text2 {
-            return State.discordanceError.value
+            return false
         }
-        return State.normal.value
+        return true
     }
 }
