380072fea409cdbe18e1099c4b6218a3f2a03b91
wansikChoi <wansook0316@gmail.com>
2021-11-29 17:09:54 +0900

    Feat: url, phone 누를시 연결되는 부분 구현

--- Escaper/Escaper/Common/Library/Validator.swift
+++ Escaper/Escaper/Common/Library/Validator.swift
@@ -9,17 +9,16 @@ import Foundation
 
 enum Validator {
     static func checkEmailFormat(text: String) -> Bool {
-        let range = NSRange(location: 0, length: text.count)
-        let pattern = "^[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}$"
-        if let idRegex = try? NSRegularExpression(pattern: pattern) {
-            let isMatched = idRegex.matches(in: text, options: [], range: range)
-            if isMatched == [] {
-                return false
-            } else {
-                return true
-            }
-        }
-        return true
+        Validator.checkWithRegex(text: text, pattern: "^[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}$")
+    }
+
+    static func checkUrlFormat(text: String) -> Bool {
+        let regex = "^(https?://)?(www\\.)?([-a-z0-9]{1,63}\\.)*?[a-z0-9][-a-z0-9]{0,61}[a-z0-9]\\.[a-z]{2,6}(/[-\\w@\\+\\.~#\\?&/=%]*)?$"
+        return Validator.checkWithRegex(text: text, pattern: regex)
+    }
+
+    static func checkTelephoneFormat(text: String) -> Bool {
+        return Validator.checkWithRegex(text: text, pattern: "^\\d{2,3}-\\d{3,4}-\\d{4}$")
     }
 
     static func checkNumberOfDigits(text: String) -> Bool {
@@ -32,6 +31,21 @@ enum Validator {
     static func checkDiscordance(text1: String, text2: String) -> Bool {
         if text1 != text2 {
             return false
+        }
+        return true
+    }
+}
+
+private extension Validator {
+    static func checkWithRegex(text: String, pattern: String) -> Bool {
+        let range = NSRange(location: 0, length: text.count)
+        if let idRegex = try? NSRegularExpression(pattern: pattern) {
+            let isMatched = idRegex.matches(in: text, options: [], range: range)
+            if isMatched == [] {
+                return false
+            } else {
+                return true
+            }
         }
         return true
     }
