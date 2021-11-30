0d93aadef68c95e7db61368cc0d58386207c0f30
wansikChoi <wansook0316@gmail.com>
2021-11-30 22:51:51 +0900

    Feat: 리더보드 불러오는 와중에 발생하는 다운로드 시점문제 해결

--- Escaper/Escaper/Infrastructure/ImageCacheManager.swift
+++ Escaper/Escaper/Infrastructure/ImageCacheManager.swift
@@ -15,34 +15,45 @@ class ImageCacheManager {
 
     static let shared = ImageCacheManager()
 
+    private let imageCacheQueue = DispatchQueue(label: "imageCache")
     private let fileManager = FileManager.default
     private let storage = Storage.storage()
-    private let cachePath = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true).first!
+    private let cachePath = URL(string: NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true)[0])
+    lazy private var cache: URLCache = {
+        let cachesURL = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask)[0]
+        let diskCacheURL = cachesURL.appendingPathComponent("DownloadedCache")
+        let cache = URLCache(memoryCapacity: 100_000_000, diskCapacity: 500_000_000, directory: diskCacheURL)
+        return cache
+    }()
+    lazy private var session: URLSession = {
+        let config = URLSessionConfiguration.default
+        config.urlCache = cache
+        return URLSession(configuration: config)
+    }()
 
     private init() {}
 
     func download(urlString: String, completion: @escaping DataCompletion) {
-        var filePath = URL(fileURLWithPath: cachePath)
-        guard let fileName = urlString.components(separatedBy: "/").last else { return }
-        filePath.appendPathComponent(fileName)
-        if !fileManager.fileExists(atPath: filePath.path) {
-            self.storage.reference(forURL: urlString).downloadURL { url, error in
-                guard let url = url else { return }
-                NetworkService.shared.downloadImage(from: url) { [weak self] result in
-                    guard let self = self else { return }
-                    switch result {
-                    case .success(let localURL):
-                        try? self.fileManager.moveItem(at: localURL, to: filePath)
-                        guard let data = try? Data(contentsOf: filePath) else { return }
-                        completion(.success(data))
-                    case .failure(let error):
-                        completion(.failure(error))
-                    }
+        self.storage.reference(forURL: urlString).downloadURL { url, error in
+            guard let url = url else { return }
+            let request = URLRequest(url: url)
+            if let data = self.cache.cachedResponse(for: request)?.data {
+                completion(.success(data))
+                return
+            }
+            let downloadTask = URLSession.shared.downloadTask(with: request) { [weak self] url, response, _ in
+                if let data = self?.cache.cachedResponse(for: request)?.data {
+                    completion(.success(data))
+                    return
+                }
+                if let response = response, let localURL = url,
+                   self?.cache.cachedResponse(for: request) == nil,
+                   let data = try? Data(contentsOf: localURL, options: [.mappedIfSafe]) {
+                    self?.cache.storeCachedResponse(CachedURLResponse(response: response, data: data), for: request)
+                    completion(.success(data))
                 }
             }
-        } else {
-            guard let data = try? Data(contentsOf: filePath) else { return }
-            completion(.success(data))
+            downloadTask.resume()
         }
     }
 }
